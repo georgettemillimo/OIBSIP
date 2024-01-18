cb64f41dc121c10cd5c40ecd41ebb82bc5b8d0b7
georgettemillimo <georgettemillimo@gmail.com>
2024-01-18 23:11:13 +0300

    Main Screen Added

--- UnitConverter/app/src/main/java/com/devmillimo/unitconverter/MainActivity.kt
+++ UnitConverter/app/src/main/java/com/devmillimo/unitconverter/MainActivity.kt
@@ -11,15 +11,23 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.devmillimo.unitconverter.ui.theme.UnitConverterTheme
+import com.devmillimo.unitconverter.ui.theme.main_screen.MainScreen
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             UnitConverterTheme {
-                // A surface container using the 'background' color from the theme
-
+                MainScreen()
             }
         }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    UnitConverterTheme {
+        MainScreen()
     }
 }
