19d5c475f948a322d329281cc094770249cbbfc8
georgettemillimo <georgettemillimo@gmail.com>
2024-01-18 21:31:43 +0300

    First commit

---
+++ UnitConverter/app/src/main/java/com/devmillimo/unitconverter/MainActivity.kt
@@ -0,0 +1,25 @@
+package com.devmillimo.unitconverter
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.devmillimo.unitconverter.ui.theme.UnitConverterTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            UnitConverterTheme {
+                // A surface container using the 'background' color from the theme
+
+            }
+        }
+    }
+}
