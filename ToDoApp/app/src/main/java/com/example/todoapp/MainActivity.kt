fdfb71ed8b740d993e816fe0b7acf7803fd78e26
georgettemillimo <georgettemillimo@gmail.com>
2024-01-22 16:54:46 +0300

    Updated Project To Do App

--- ToDoApp/app/src/main/java/com/example/todoapp/MainActivity.kt
+++ ToDoApp/app/src/main/java/com/example/todoapp/MainActivity.kt
@@ -3,13 +3,7 @@ package com.example.todoapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import com.example.todoapp.main.MainApp
 import com.example.todoapp.ui.theme.ToDoAppTheme
 
 class MainActivity : ComponentActivity() {
@@ -18,12 +12,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             ToDoAppTheme {
                 // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-
-                }
+                MainApp ()
             }
         }
     }
---
+++ ToDoApp/app/src/main/java/com/example/todoapp/main/Register.kt
@@ -0,0 +1,25 @@
+package com.example.todoapp.main
+
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.material3.Surface
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+
+@Composable
+
+fun Register(){
+
+    Surface (modifier = Modifier.fillMaxHeight(),
+        color = Color.White) {
+
+    }
+}
+
+@Preview
+@Composable
+
+fun DefaultPreviewofRegister(){
+    Register()
+}
